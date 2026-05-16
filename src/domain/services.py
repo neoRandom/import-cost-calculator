8c72acc703ecb2ba3ccbd6535433e7a2099e83fa
Fellipe Leonardo <112525075+neoRandom@users.noreply.github.com>
2026-05-15 23:46:16 -0300

    style(services.py): more readable variables
    dollar -> dollar_rate
    
    quota -> quota_value

--- src/domain/services.py
+++ src/domain/services.py
@@ -2,20 +2,20 @@ from domain.models import PersonalImportingCase, TraditionalImportingCase
 
 
 class ImportingCostCalculator:
-    def __init__(self, dollar: float, quota: float):
-        self.dollar = dollar
-        self.quota = quota
+    def __init__(self, dollar_rate: float, quota_value: float):
+        self.dollar_rate = dollar_rate
+        self.quota_value = quota_value
 
     def dollar_to_reais(self, value: float) -> float:
-        return value * self.dollar
+        return value * self.dollar_rate
 
     def calculate_traditional_importing_cost(self, case: TraditionalImportingCase) -> float:
         return self.dollar_to_reais(case.value) * 2
 
     def calculate_personal_importing_cost(self, case: PersonalImportingCase) -> float:
         if case.is_declaring:
-            return self.dollar_to_reais(case.value) + (self.dollar_to_reais(case.value - self.quota) * 0.5)
+            return self.dollar_to_reais(case.value) + (self.dollar_to_reais(case.value - self.quota_value) * 0.5)
         elif case.is_clean:
             return self.dollar_to_reais(case.value)
 
-        return self.dollar_to_reais(case.value) + self.dollar_to_reais(case.value - self.quota)
+        return self.dollar_to_reais(case.value) + self.dollar_to_reais(case.value - self.quota_value)
